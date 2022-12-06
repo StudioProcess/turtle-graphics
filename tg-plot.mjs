b379efc6642a7607d9c7204436d8afdb6388d393
Martin Grödl <martin@process.studio>
2022-12-06 17:08:19 +0000

    Format numbers in preview

--- tg-plot.mjs
+++ tg-plot.mjs
@@ -679,7 +679,7 @@ export function make_plotter_client(tg_instance) {
             console.warn(`🖨️ → Preview window was blocked. Please allow the pop-up in your browser and try again!`);
             return;
         }
-        w.document.write(`<html><head><title>${filename}</title></head><body style="padding:0; margin:0; font:10px system-ui; color:dimgray; background:lightgray; display:flex; flex-direction:column; align-items:center; justify-content:center;"><div><img src="${url}" style="background:white; max-width:90vw; max-height:80vh; box-shadow:3px 3px 10px 1px gray;" /><div style="margin-top:2em;">${filename}<br>${stats.count} lines, ${Math.floor(stats.travel_ink)}/${Math.floor(stats.travel)} mm<br>${format_name} (${size[0]} ✕ ${size[1]} mm)<br><a href="${url}" download="${filename}" style="color:dimgray;">Download</a></div></div></body></html>`);
+        w.document.write(`<html><head><title>${filename}</title></head><body style="padding:0; margin:0; font:10px system-ui; color:dimgray; background:lightgray; display:flex; flex-direction:column; align-items:center; justify-content:center;"><div><img src="${url}" style="background:white; max-width:90vw; max-height:80vh; box-shadow:3px 3px 10px 1px gray;" /><div style="margin-top:2em;">${filename}<br>${format_num(stats.count)} lines<br>${format_num(Math.floor(stats.travel_ink))} / ${format_num(Math.floor(stats.travel))} mm<br>${format_name} (${size[0]} ✕ ${size[1]} mm)<br><a href="${url}" download="${filename}" style="color:dimgray;">Download</a></div></div></body></html>`);
     };
     
     savesvg_button.onmousedown = async (e) => {
